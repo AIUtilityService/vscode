efab916e81e5047511efd46f4d4fae5b1d588e4c
Himanshu Sharma <himanshu.k@sellergeni.com>
2025-03-06 19:56:47 +0530

    added different color for the CEL expression

--- src/providers/celSyntax.ts
+++ src/providers/celSyntax.ts
@@ -4,52 +4,54 @@ export class CELSyntaxProvider
   implements vscode.DocumentSemanticTokensProvider
 {
   private readonly tokenTypes = new Map<string, number>([
-    ["celExpression", 0],
-    ["variable", 1],
-    ["operator", 2],
-    ["function", 3],
+    ["celRoot", 0],
+    ["celField", 1],
+    ["celLeafField", 2],
   ]);
 
-  private readonly tokenModifiers = new Map<string, number>([
-    ["declaration", 0],
-    ["readonly", 1],
-  ]);
-
-  private readonly legend = new vscode.SemanticTokensLegend(
-    [...this.tokenTypes.keys()],
-    [...this.tokenModifiers.keys()]
-  );
+  private readonly tokenModifiers = new Map<string, number>();
 
   public getLegend(): vscode.SemanticTokensLegend {
-    return this.legend;
+    return new vscode.SemanticTokensLegend(
+      [...this.tokenTypes.keys()],
+      [...this.tokenModifiers.keys()]
+    );
   }
 
   async provideDocumentSemanticTokens(
     document: vscode.TextDocument
   ): Promise<vscode.SemanticTokens> {
-    const builder = new vscode.SemanticTokensBuilder(this.legend);
+    const builder = new vscode.SemanticTokensBuilder(this.getLegend());
 
     for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
       const line = document.lineAt(lineIndex);
       const text = line.text;
 
-      // Find CEL expressions in the line
       const matches = text.match(/\${[^}]+}/g);
       if (matches) {
         matches.forEach((match) => {
           const startIndex = text.indexOf(match);
           if (startIndex > -1) {
-            // Highlight the entire CEL expression
-            builder.push(
-              lineIndex,
-              startIndex,
-              match.length,
-              this.tokenTypes.get("celExpression")!,
-              0
-            );
+            // Remove ${ and } from the match
+            const expression = match.slice(2, -1);
+            const parts = expression.split(".");
 
-            // Additional parsing for internal CEL components
-            // You can add more sophisticated parsing here
+            let currentIndex = startIndex + 2; // Skip ${
+
+            parts.forEach((part, index) => {
+              let tokenType;
+              if (index === 0) {
+                tokenType = this.tokenTypes.get("celRoot")!;
+              } else if (index === parts.length - 1) {
+                tokenType = this.tokenTypes.get("celLeafField")!;
+              } else {
+                tokenType = this.tokenTypes.get("celField")!;
+              }
+
+              builder.push(lineIndex, currentIndex, part.length, tokenType, 0);
+
+              currentIndex += part.length + 1; // +1 for the dot
+            });
           }
         });
       }
